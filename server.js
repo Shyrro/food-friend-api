24c862e18de620858c31fe71129315d4cdc75241
Shyrro <zsahmane@gmail.com>
2021-07-19 00:28:58 +0200

    test add heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/food", function (req, res) {
-  return res.send(["tomato", "cucumber", "carrot"]);
+  return res.send(["tomato", "cucumber", "carrot", "oignon"]);
 });
 
 app.listen(process.env.PORT || 8080);
