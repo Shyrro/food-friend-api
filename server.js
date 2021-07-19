912cbaf911f92a788800a7a54be18584a3b4d3e0
Shyrro <zsahmane@gmail.com>
2021-07-20 00:15:54 +0200

    feat(api): add ingredients endpoint
    Signed-off-by: Shyrro <zsahmane@gmail.com>

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 const express = require("express");
 const app = express();
 
-app.get("/food", function (req, res) {
-  return res.send(["tomato", "cucumber", "carrot", "oignon"]);
+const { MongoClient } = require("mongodb");
+require("dotenv").config({ path: __dirname + "/.env" });
+const uri = `mongodb+srv://${process.env["DB_USERNAME"]}:${process.env["DB_PASSWORD"]}@mycluster.g0vxx.mongodb.net/${process.env["DB_NAME"]}?retryWrites=true&w=majority`;
+
+const connectToDb = async () => {
+  const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  if (!client.isConnected)
+    await client.connect().catch((err) => console.log(err));
+  return client;
+};
+
+app.get("/ingredients", async function (req, res) {
+  const client = await connectToDb();
+
+  const collection = client.db("FoodPlanner").collection("Ingredients");
+  collection.find({}).toArray(function (err, ingredients) {
+    if (err) throw err;
+
+    return res.send(ingredients);
+  });
 });
 
 app.listen(process.env.PORT || 8080);
