23f53bc41cfabf79fe2d4f002f6af1f8d91eee03
Shyrro <zsahmane@gmail.com>
2021-07-23 21:52:48 +0200

    feat(api): add cors
    Signed-off-by: Shyrro <zsahmane@gmail.com>

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
+app.use(cors());
 
 const { MongoClient } = require("mongodb");
 require("dotenv").config({ path: __dirname + "/.env" });
